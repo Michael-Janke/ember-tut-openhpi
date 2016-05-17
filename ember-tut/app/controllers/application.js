ae6ea85957c66b98889a70e312ae2f509176a36c
Sören Oldag <soeren_oldag@freenet.de>
2016-05-17 16:17:56 +0200

    Add switch wormhole destination button.

--- ember-tut/app/controllers/application.js
+++ ember-tut/app/controllers/application.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
+	destinationId: "questions",
+	actions: {
+		switchWormhole: function() {
+			if(this.get("destinationId") == "questions") {
+				this.set("destinationId", "sidebar");
+			}
+			else {
+				this.set("destinationId", "questions");
+			}
+		}
+	}
 });
